642581f80913cc90e98fd24138ac0b7dabf4b875
Melanie <melanieoh888@gmail.com>
2016-11-29 02:41:21 +0000

    seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create({ username: "sharky_j", avatar_url: "http://naserca.com/images/sharky_j.jpg", 
-photo_url: "http://naserca.com/images/shark.jpg", user_id: user.id }) 
+user = User.create({ username: "sharky_j", avatar_url: "http://naserca.com/images/sharky_j.jpg" }) 
+Post.create({ user_id: user.id, photo_url: "http://naserca.com/images/shark.jpg"}) 
 
-Comment.create({ user_id: 1, post_id: 1, text: "This is awesome!" })
+Comment.create({ user_id: user.id, post_id: 1, text: "This is awesome!" })
 
 Like.create({ user_id: 1, post_id: 1})
 
-User.create({ username: "kirk_whalum", avatar_url: "http://naserca.com/images/whale.jpg", 
-photo_url: "http://naserca.com/images/whale.jpg", user_id: user.id })
+user2 = User.create({ username: "kirk_whalum", avatar_url: "http://naserca.com/images/whale.jpg" }) 
+Post.create({ user_id: user2.id, photo_url: "http://naserca.com/images/whale.jpg" })
 
-User.create({ username: "marlin_peppa", avatar_url: "http://naserca.com/images/marlin_peppa.jpg", 
-photo_url: "http://naserca.com/images/marlin.jpg", user_id: user_id })
+user3 = User.create({ username: "marlin_peppa", avatar_url: "http://naserca.com/images/marlin_peppa.jpg" }) 
+Post.create({ user_id: user3.id, photo_url: "http://naserca.com/images/marlin.jpg" })
